cbab5f223314defc7f18c82ecf24912070d54a1f
ibrahim hisham <muhammadibrahimhisham@gmail.com>
2026-03-28 11:46:00 +0500

    feat: add installation section with upm urls and copy button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Features from './components/Features'
+import Installation from './components/Installation'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <Features />
+      <Installation />
     </div>
   )
 }
